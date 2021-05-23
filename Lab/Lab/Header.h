f7d88afa9f25a70daae23fbdc142b12e9ca1bf43
Dara-Klochan <dara.klochan@yandex.ru>
2021-05-23 21:08:48 +0700

    2Lab_New_Class

--- Lab/Lab/Header.h
+++ Lab/Lab/Header.h
@@ -53,6 +53,12 @@ public:
 	Diagonal_matrix() {};
 };
 
-
+class Triangular_matrix : public Matrix {
+	int* Array;
+public:
+	void In_Array(int N, ifstream& ifst); //Функция ввода массива
+	void Out_Array(int N, ofstream& ofst); //Функция вывода массива
+	Triangular_matrix() {};
+};
 
 #endif //HEADER_H
--- Lab/Lab/Source.cpp
+++ Lab/Lab/Source.cpp
@@ -67,6 +67,9 @@ Matrix* Matrix::In_Matrix(ifstream& ifst) {
     else if (K == 2) {
         M = new Diagonal_matrix(); //Создаем диагональную матрицу
     }
+    else if (K == 3) {
+        M = new Triangular_matrix(); //Создаем треугольную матрицу
+    }
     else {
         return 0;
     }
@@ -128,6 +131,43 @@ void Diagonal_matrix::Out_Array(int N, ofstream& ofst)
             }
             else {
                 ofst << "0 "; //(i != j) -> не диагональ, выводим нули
+            }
+        }
+
+        ofst << endl;
+    }
+}
+
+void Triangular_matrix::In_Array(int N, ifstream& ifst) {
+    int Temp_N = N;
+    int Array_size = 0;
+
+    while (Temp_N) {
+        Array_size += Temp_N;
+        Temp_N--;
+    }
+
+    Array = new int[Array_size];
+
+    for (int i = 0; i < Array_size; i++) {
+        ifst >> Array[i];
+    }
+}
+
+void Triangular_matrix::Out_Array(int N, ofstream& ofst) {
+    ofst << "It's triangular matrix with dimension = " << N << endl;
+
+    int index = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+        {
+            if (i >= j) {
+                ofst << Array[index] << " ";
+                index++;
+            }
+            else {
+                ofst << 0 << " ";
             }
         }
 
