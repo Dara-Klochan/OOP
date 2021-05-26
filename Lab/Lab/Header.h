681769fc79b5462c510952d4d3c38cb39a936eac
Dara-Klochan <dara.klochan@yandex.ru>
2021-05-26 13:09:02 +0700

    2Lab_Sort_New_Class_New_Field

--- Lab/Lab/Header.h
+++ Lab/Lab/Header.h
@@ -5,15 +5,24 @@
 
 using namespace std;
 
+//Способ вывода матрицы
+enum Key_Out {
+	BY_LINE, //По строкам
+	BY_COLUMN, //По столбцам
+	ONE_DIMENSIONAL //В виде одномерного массива
+};
+
 class Matrix {
 	int N;
+	Key_Out K_O;
 public:
 	int Get_N(); //Функция получения размерности массива (общее значение)
+	Key_Out Get_K_O(); //Функция получения способа вывода матрицы
 
 	static Matrix* In_Matrix(ifstream& ifst); //Функция ввода матрицы
 	virtual void In_Array(int N, ifstream& ifst) = 0; //Чисто вирутальная функция ввода матрицы,
 											  //она будет определена каждого класса массива
-	virtual void Out_Array(int N, ofstream& ofst) = 0; //Чисто вирутальная функция вывода матрицы,
+	virtual void Out_Array(int N, Key_Out K_O, ofstream& ofst) = 0; //Чисто вирутальная функция вывода матрицы,
 											  //она будет определена каждого класса массива
 	virtual int Sum(int N) = 0; //Функция подсчета суммы элементов матрицы
 	bool Compare(Matrix* Other); //Функция сравнения сумм элементов матриц
@@ -44,7 +53,7 @@ class Two_dimensional_array : public Matrix {
 	int** Array; //Массив
 public:
 	void In_Array(int N, ifstream& ifst);  //Функция ввода массива
-	void Out_Array(int N, ofstream& ofst); //Функция вывода массива
+	void Out_Array(int N, Key_Out K_O, ofstream& ofst); //Функция вывода массива
 	int Sum(int N); //Функция подсчет суммы элементов матрицы
 	Two_dimensional_array() {};
 };
@@ -53,7 +62,7 @@ class Diagonal_matrix : public Matrix {
 	int* Array; //Массива
 public:
 	void In_Array(int N, ifstream& ifst); //Функция ввода массива
-	void Out_Array(int N, ofstream& ofst); //Функция вывода массива
+	void Out_Array(int N, Key_Out K_O, ofstream& ofst); //Функция вывода массива
 	int Sum(int N); //Функция подсчет суммы элементов матрицы
 	Diagonal_matrix() {};
 };
@@ -62,7 +71,7 @@ class Triangular_matrix : public Matrix {
 	int* Array;
 public:
 	void In_Array(int N, ifstream& ifst); //Функция ввода массива
-	void Out_Array(int N, ofstream& ofst); //Функция вывода массива
+	void Out_Array(int N, Key_Out K_O, ofstream& ofst); //Функция вывода массива
 	int Sum(int N); //Функция подсчет суммы элементов матрицы
 	Triangular_matrix() {};
 };
